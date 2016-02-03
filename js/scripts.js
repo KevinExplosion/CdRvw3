75649dec9a95059d09d70826f3a3b3227472728c
kevin <mattisonii@yahoo.com>
2016-02-02 18:18:09 -0800

    Change append to text in result display.

--- js/scripts.js
+++ js/scripts.js
@@ -26,19 +26,8 @@ $(document).ready(function() {
     var result = pingPong(number);
     var pingPongArray = [];
 
-    for(var i = 1; i <= number; i++) {
-        if (i % 15 === 0) {
-          pingPongArray.push("pingpong");
-        } else if (i % 5 === 0){
-          pingPongArray.push("pong");
-        } else if (i % 3 === 0) {
-          pingPongArray.push("ping");
-        } else {
-          pingPongArray.push(i);
-        }
-    } $("#resultP").append("<blockquote>" + pingPongArray + "</blockquote>");
+   $("#result").text(result);
 
   $("#resultDisplay").show();
-
   });
 });
--- spec/specs.js
+++ spec/specs.js
@@ -1,17 +1,17 @@
 describe('pingPong', function() {
   it("will return ping if the input is divisible by 3", function() {
-    expect(pingPong(3)).to.equal("ping");
+    expect(pingPong(3)).to.eql([ 1, 2, 'ping' ]);
   });
 
   it("will return pong if the input is divisible by 5", function() {
-    expect(pingPong(5)).to.equal("pong");
+    expect(pingPong(5)).to.eql([ 1, 2, 'ping', 4, 'pong' ]);
   });
 
   it("will return pingpong if the input is divisible by 3 and 5", function() {
-    expect(pingPong(15)).to.equal("pingpong");
+    expect(pingPong(15)).to.eql[ Array(15) ];
   });
 
   it("will return the given input if it is not divisible by 3 or 5", function(){
-    expect(pingPong(2)).to.equal(2);
+    expect(pingPong(2)).to.eql([ 1, 2 ]);
   });
 });
